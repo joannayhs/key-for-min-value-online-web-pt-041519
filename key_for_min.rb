711bb783c9047fde1187ed6f0cafc9d34793ac26
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-23 14:17:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,14 @@
 require "pry"
 
 def key_for_min_value(name_hash)
-  lowest_value = nil
+  lowest_value = nil 
+  #allows first iteration to be true or else nil 
   lowest_key = nil 
   name_hash.each do |key, value|
-    if lowest_value == nil || value < lowest_value #compares each value to the one before
-      lowest_value = value #sets the previous value as the value to compare each iteration to 
+    if lowest_value == nil || value < lowest_value 
+      #compares each value to the one before
+      lowest_value = value 
+      #sets the previous value as the value to compare to 
       lowest_key = key
     end
   end
