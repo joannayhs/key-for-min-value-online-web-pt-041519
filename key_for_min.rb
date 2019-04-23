fb2b9ddf1ab73388e329a4559efc45ba2998ae62
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-23 14:15:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   lowest_key = nil 
   name_hash.each do |key, value|
-    if lowest_value == nil || value < lowest_value 
+    if lowest_value == nil || value < lowest_value #t
       lowest_value = value
       lowest_key = key
     end
